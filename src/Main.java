a994e79b7949a07ee2156c915f7a5a355155b21e
Ofir Abramovich <ofir1080@gmail.com>
2018-07-22 00:34:48 +0300

    added auther

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
  * while working on the algorithms I had to use basic mathematics and physics.
  * I'm sure there are more officiant ways implementing them, but it was important for me to build everything
  * from scratch. Enjoy!
+ *
+ * @author Ofir Abramovich
  */
 
 public class Main {
